0bd2bfcf37558cf0b69903046792a491a3f5a848
Jeremy Cherer <chererjeremy@gmail.com>
2023-07-14 11:06:59 -0600

    hmm

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ fastify.get('/sms-2fa', async function handler (request, reply) {
 
 // Run the server!
 try {
+  console.log('Starting server...')
   await fastify.listen({ port: 3000 })
 } catch (err) {
   fastify.log.error(err)
